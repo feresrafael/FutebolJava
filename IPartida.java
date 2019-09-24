52f91c6a4b7fa1cea2deb8e9dcaa6794d6d5bf97
feresrafael <rafael.feres@soulasalle.com.br>
2019-09-24 11:03:42 -0300

    Add files via upload

---
+++ IPartida.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package futebol;
+
+/**
+ *
+ * @author feres
+ */
+public interface IPartida {
+
+	public abstract void iniciarPartida();
+	public abstract void mostrarResultado();
+	public abstract void finalizarPartida();
+
+
+}
